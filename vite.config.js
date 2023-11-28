cd5b7d7e46eebe6ecede20b9a1ccbaa09c5e45ce
M Atif <chatif476@gmail.com>
2023-11-29 01:20:18 +0500

    Update package.json and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
+import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -21,6 +22,21 @@ export default defineConfig({
       dirs: ["./src/examples"],
     }),
   ],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/directives.js"),
+      name: "direvue",
+      fileName: (format) => `direvue.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
   resolve: {
     alias: {
       "@": "/src",
