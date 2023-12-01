fa43db0f6cdbdaa0f04e640f3ed67d07ba69b1f5
M Atif <chatif476@gmail.com>
2023-12-02 01:28:39 +0500

    Add favicon and meta tags for SEO

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -3,6 +3,67 @@ import { defineConfig } from "vitepress";
 export default defineConfig({
   title: "DireVue",
   description: "Your Go-To Toolkit of Vue Directives",
+  head: [
+    [
+      "link",
+      {
+        rel: "icon",
+        href: "https://gist.githubusercontent.com/atif0075/7a7cda43c49481bc7141d4e7224579f6/raw/6a1701b9a0bda46fcc7405c7b4a1b701aa3b6ee8/direvue.svg",
+      },
+    ],
+    [
+      "meta",
+      {
+        name: "google-site-verification",
+        content: "ReoTTcKgUwVb6_Ls2uHpr7AnrGBZq_Gf8k7dvKm_Y3c",
+      },
+    ],
+    [
+      "meta",
+      {
+        name: "keywords",
+        content:
+          "vue, vuejs, vue3, vue3-directives, vue-directives, vue3-directive, vue-directive, vue3-directives, vue-directives, vue3-directive, vue-directive, vue3-directives, vue-directives, vue3-directive, vue-directive, vue3-directives, vue-directives, vue3-directive, vue-directive, vue3-directives, vue-directives, vue3-directive, vue-directive, vue3-directives, vue-directives, vue3-directive, vue-directive, vue3-directives, vue-directives, vue3-directive, vue-directive, vue3-directives, vue-directives, vue3-directive, vue-directive",
+      },
+    ],
+    [
+      "meta",
+      {
+        name: "description",
+        content:
+          "DireVue is a comprehensive collection of Vue directives designed to enhance your Vue.js development experience. With a focus on simplicity, flexibility, and efficiency, DireVue empowers you to build your next Vue app faster and with less effort.",
+      },
+    ],
+    [
+      "meta",
+      {
+        name: "og:url",
+        content: "https://direvue.vercel.app/",
+      },
+    ],
+    [
+      "meta",
+      {
+        name: "og:image",
+        content:
+          "https://gist.githubusercontent.com/atif0075/7a7cda43c49481bc7141d4e7224579f6/raw/6a1701b9a0bda46fcc7405c7b4a1b701aa3b6ee8/direvue.svg",
+      },
+    ],
+    [
+      "meta",
+      {
+        name: "og:type",
+        content: "website",
+      },
+    ],
+    [
+      "meta",
+      {
+        name: "og:locale",
+        content: "en_US",
+      },
+    ],
+  ],
   themeConfig: {
     logo: "https://gist.githubusercontent.com/atif0075/7a7cda43c49481bc7141d4e7224579f6/raw/6a1701b9a0bda46fcc7405c7b4a1b701aa3b6ee8/direvue.svg",
     // https://vitepress.dev/reference/default-theme-config
