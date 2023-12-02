436b9c445866c3a906421d1ceef764b51598b7e6
M Atif <chatif476@gmail.com>
2023-12-03 04:45:27 +0500

    Refactor image comparison directive

--- code/image/v-image-compare.js
+++ code/image/v-image-compare.js
@@ -1,22 +1,40 @@
 export const vImageCompare = {
   mounted(el, binding) {
+    console.log(binding.value);
     const container = document.createElement("div");
-    const imgWrapperBefore = document.createElement("div");
-    imgWrapperBefore.classList.add("img-wrapper", "before");
-    imgWrapperBefore.style.backgroundImage = `url(${binding.value.before})`;
+    container.classList.add("image-compare-container");
 
-    const imgWrapperAfter = document.createElement("div");
+    const imgWrapperBefore = document.createElement("img");
+    imgWrapperBefore.classList.add("img-wrapper", "before");
+    binding.value.customClassSuffix
+      ? imgWrapperBefore.classList.add(
+          "before-" + binding.value.customClassSuffix
+        )
+      : null;
+    imgWrapperBefore.src = binding.value.before;
+
+    const imgWrapperAfter = document.createElement("img");
     imgWrapperAfter.classList.add("img-wrapper", "after");
-    imgWrapperAfter.style.backgroundImage = `url(${binding.value.after})`;
+    console.log(binding.value.customClassSuffix);
+    binding.value.customClassSuffix
+      ? imgWrapperAfter.classList.add(
+          "after-" + binding.value.customClassSuffix
+        )
+      : null;
+    imgWrapperAfter.src = binding.value.after;
 
     const slider = document.createElement("div");
-    slider.classList.add("slider");
+    binding.value.direction === "vertical"
+      ? slider.classList.add("slider", "slider-vertical")
+      : slider.classList.add("slider");
 
     const handle = document.createElement("div");
-    // handle.classList.add("handle");
+    handle.classList.add("handle");
     handle.innerHTML =
-      '<svg xmlns="http://www.w3.org/2000/svg" class="handle" viewBox="-8 -3 16 6"> <path d="M -5 -2 L -7 0 L -5 2 M 5 -2 L 7 0 L 5 2" fill="none" vector-effect="non-scaling-stroke"></path> </svg>';
-
+      '<svg xmlns="http://www.w3.org/2000/svg" viewBox="-8 -3 16 6"> <path d="M -5 -2 L -7 0 L -5 2 M 5 -2 L 7 0 L 5 2" fill="none" vector-effect="non-scaling-stroke"></path> </svg>';
+    if (binding.value.direction === "vertical") {
+      handle.classList.add("handle-vertical");
+    }
     slider.appendChild(handle);
     container.appendChild(imgWrapperBefore);
     container.appendChild(imgWrapperAfter);
@@ -27,13 +45,32 @@ export const vImageCompare = {
     let isSliding = false;
 
     const debounce = (func, delay) => {
-      let timeout;
-      return function () {
-        const context = this;
-        const args = arguments;
-        clearTimeout(timeout);
-        timeout = setTimeout(() => func.apply(context, args), delay);
-      };
+      if (binding.value.direction === "vertical") {
+        let timeoutX, timeoutY;
+
+        return function () {
+          const context = this;
+          const args = arguments;
+
+          // Use different timeouts for vertical and vertical sliders
+          if (args[0].type === "mousemove") {
+            clearTimeout(timeoutX);
+            timeoutX = setTimeout(() => func.apply(context, args), delay);
+          } else if (args[0].type === "mousedown") {
+            clearTimeout(timeoutY);
+            timeoutY = setTimeout(() => func.apply(context, args), delay);
+          }
+        };
+      } else {
+        let timeout;
+        return function () {
+          const context = this;
+          const args = arguments;
+
+          clearTimeout(timeout);
+          timeout = setTimeout(() => func.apply(context, args), delay);
+        };
+      }
     };
 
     const startSlide = () => {
@@ -43,29 +80,45 @@ export const vImageCompare = {
     const endSlide = () => {
       isSliding = false;
     };
+
+    const setSliderPosition = (percentage) => {
+      if (binding.value.direction === "vertical") {
+        slider.style.top = `${percentage}%`;
+        // slider.style.transform = `translate(-50%, -50%) rotate(90deg)`;
+        imgWrapperBefore.style.clipPath = `inset(${percentage}% 0 0 0)`;
+        imgWrapperAfter.style.clipPath = `inset(0 0  ${100 - percentage}% 0)`;
+      } else {
+        slider.style.left = `${percentage}%`;
+        imgWrapperBefore.style.clipPath = `inset(0 ${100 - percentage}% 0 0)`;
+        imgWrapperAfter.style.clipPath = `inset(0 0 0 ${percentage}%)`;
+        // inset(0 0 50% 0);
+      }
+    };
+
     if (binding.value.sliderValue) {
-      slider.style.left = `${binding.value.sliderValue}%`;
-      imgWrapperBefore.style.width = `${binding.value.sliderValue}%`;
-      // change min-width too
-      imgWrapperBefore.style.minWidth = `${binding.value.sliderValue}%`;
+      setSliderPosition(binding.value.sliderValue);
     } else {
-      // default slider value to 50%
-      slider.style.left = `50%`;
-      imgWrapperBefore.style.width = `50%`;
-      // change min-width too
-      imgWrapperBefore.style.minWidth = `50%`;
+      setSliderPosition(50);
     }
 
     const slide = debounce((e) => {
       if (isSliding) {
         const rect = container.getBoundingClientRect();
-        let x = e.clientX - rect.left;
-        x = Math.max(0, Math.min(x, rect.width));
-        const percentage = (x / rect.width) * 100;
-        slider.style.left = `${percentage}%`;
-        imgWrapperBefore.style.width = `${percentage}%`;
-        // change min-width too
-        imgWrapperBefore.style.minWidth = `${percentage}%`;
+        let position;
+
+        if (binding.value.direction === "vertical") {
+          position = e.clientY - rect.top;
+          position = Math.max(0, Math.min(position, rect.height));
+
+          const percentage = (position / rect.height) * 100;
+          setSliderPosition(percentage);
+        } else {
+          position = e.pageX - rect.left;
+          position = Math.max(0, Math.min(position, rect.width));
+
+          const percentage = (position / rect.width) * 100;
+          setSliderPosition(percentage);
+        }
       }
     }, 10);
 
--- src/examples/image/ImageCompare.vue
+++ src/examples/image/ImageCompare.vue
@@ -1,36 +1,38 @@
 <script setup>
 const compareData = [
   {
+    name: "horizontal compare",
     before:
-      "https://images.pexels.com/photos/2928178/pexels-photo-2928178.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      "https://images.pexels.com/photos/4344259/pexels-photo-4344259.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
     after:
-      "https://images.pexels.com/photos/2928178/pexels-photo-2928178.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      "https://images.pexels.com/photos/4344267/pexels-photo-4344267.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
     sliderValue: 50,
   },
   {
+    name: "vertical compare",
     before:
-      "https://images.pexels.com/photos/4895059/pexels-photo-4895059.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      "https://images.pexels.com/photos/3754814/pexels-photo-3754814.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     after:
-      "https://images.pexels.com/photos/4895059/pexels-photo-4895059.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      "https://images.pexels.com/photos/3754814/pexels-photo-3754814.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     sliderValue: 30,
+    direction: "vertical", // default is horizontal
+    customClassSuffix: "gray", // => before-gray and after-gray, just to add some styles
   },
 ];
 </script>
 
 <template>
-  <main
-    class="grid grid-cols-1 sm:grid-cols-2 gap-4 items-center justify-center"
-  >
-    <div
-      v-for="(item, index) in compareData"
-      :key="index"
-      class="w-72 h-56 relative rounded-2xl overflow-hidden"
-    >
+  <main class="grid grid-cols-1 items-center justify-center">
+    <div v-for="(item, index) in compareData" :key="index">
+      <h3 class="capitalize pb-2">{{ item.name }}</h3>
       <div
+        class="rounded-2xl overflow-hidden"
         v-image-slider="{
           before: item.before,
           after: item.after,
           sliderValue: item.sliderValue,
+          direction: item.direction,
+          customClassSuffix: item.customClassSuffix,
         }"
       ></div>
     </div>
@@ -38,31 +40,33 @@ const compareData = [
 </template>
 
 <style>
-.img-comparison-container {
+.image-compare-container {
+  box-sizing: border-box;
   position: relative;
+  display: flex;
+  /* flex-direction: column; */
   overflow: hidden;
   width: 100%;
-  height: 100%;
 }
 
-.img-wrapper {
-  width: 100%;
-  height: 100%;
-  object-fit: contain;
-}
-
-.handle {
+.after {
+  display: flex;
   position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  cursor: pointer;
+  object-fit: cover;
+  height: 100%;
+  width: 100%;
 }
 
-.img-comparison-container,
-.img-wrapper,
-.slider,
-.handle {
-  box-sizing: border-box;
+.before,
+.after {
+  user-select: none;
+}
+
+.handle-vertical {
+  rotate: 90deg;
+  transform: translateX(-50%);
+}
+.after-gray {
+  filter: grayscale(100%);
 }
 </style>
